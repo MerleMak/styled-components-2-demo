aa1fad774131cb0026332aea09d56f1718fddfe8
Merle Makoben <merle.makoben@neuefische.de>
2024-01-22 10:14:07 +0100

    demo ffm-web-23-5

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import styled from "styled-components";
+import Button from "../components/Button";
+
+const FlexContainer = styled.section`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  // it's possible to set a default value for a prop, like in this example with $direction = "row"
+  flex-direction: ${({ $direction = "row" }) =>
+    $direction === "row" ? "row" : "column"};
+`;
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Styled Components with Props</h1>
+      <h2>Props using ternary</h2>
+      <FlexContainer>
+        <Button>Button</Button>
+        <Button $backgroundcolor="danger">Button Danger</Button>
+      </FlexContainer>
+      <h2>Using Css-Block (Complex Component)</h2>
+      <FlexContainer $direction="column">
+        <Button $variant="text">Button - Text</Button>
+        <Button $variant="outlined">Button - Outlined</Button>
+        <Button $variant="contained">Button - Contained</Button>
+      </FlexContainer>
     </div>
   );
 }
--- styles.js
+++ styles.js
@@ -11,4 +11,10 @@ export default createGlobalStyle`
     margin: 0;
     font-family: system-ui;
   }
+
+  :root {
+    --color-primary: #364F6B;
+    --color-secondary: #3FC1C9;
+    --color-danger: #ff0000;
+  }
 `;
